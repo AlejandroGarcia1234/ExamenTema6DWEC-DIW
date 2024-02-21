136b72f0a78f5f7cd83416889938a603c623c47f
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2024-02-21 10:10:31 +0100

    Le damos estilos a la galeria y vamos creando la funcion de mostrarImagen

--- build/js/index.js
+++ build/js/index.js
@@ -34,4 +34,38 @@ document.addEventListener("DOMContentLoaded", function () {
        // Agrega la imagen a la galería
       galeria.appendChild(imagen);
     }
-  } 
+  }
+
+  function mostrarImagen(id) {
+    const imagen = document.createElement("picture");
+  
+    imagen.innerHTML = `
+    <source srcset="build/img/big/${id}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/big/${id}.jpg" alt="imagen big">`;
+  
+    // Crear Overlay con la imagen
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+    overlay.onclick = function () {
+      const body = document.querySelector("body");
+      body.classList.remove("fijar-body");
+      overlay.remove();
+    };
+  
+    // Botón para cerrar el Modal
+    const cerrarModal = document.createElement("P");
+    cerrarModal.textContent = "X";
+    cerrarModal.classList.add("btn-cerrar");
+    cerrarModal.onclick = function () {
+      const body = document.querySelector("body");
+      body.classList.remove("fijar-body");
+      overlay.remove();
+    };
+    overlay.appendChild(cerrarModal);
+  
+    // Añadirlo al HTML
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body");
+  }
