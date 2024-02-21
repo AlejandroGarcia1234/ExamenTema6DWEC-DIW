05b1084913b38692388224f690bd865d5f43e7c0
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2024-02-21 10:00:41 +0100

    Añadimos los estilos al video además de alguna media querie y creamos la funcion crearGaleria

--- build/js/index.js
+++ build/js/index.js
@@ -16,3 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
     // Le añadimos al span el año actual
     yearSpan.textContent = currentYear;
   }
+
+  function crearGaleria() {
+    const galeria = document.querySelector(".galeria-imagenes");
+  
+    for (let i = 1; i <= 6; i++) {
+       // Crea un nuevo elemento de imagen
+      const imagen = document.createElement("picture");
+  
+      imagen.innerHTML = `
+      <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+      <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen thumb">`;
+  
+      imagen.onclick = function () {
+        mostrarImagen(i);
+      };
+       // Agrega la imagen a la galería
+      galeria.appendChild(imagen);
+    }
+  } 
